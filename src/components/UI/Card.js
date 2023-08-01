f45476ababa88628f19c570fb21943a9290fe52a
Charles Damasceno <swirfneblin@gmail.com>
2023-08-01 02:44:21 -0300

    adduser component

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,5 +1,9 @@
-const Card = () => {
-  return <div className="card"></div>;
+import classes from "./Card.module.css";
+
+const Card = (props) => {
+  return (
+    <div className={`${classes.card} ${props.className}`}>{props.children}</div>
+  );
 };
 
 export default Card;
--- src/components/User/AddUser.js
+++ src/components/User/AddUser.js
@@ -1,7 +1,22 @@
 import classes from "./AddUser.module.css";
+import Card from "../UI/Card";
 
-const AddUser = () => {
-  return <div></div>;
+const AddUser = (props) => {
+  const addUserhandler = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <Card className={classes.input}>
+      <form onSubmit={addUserhandler}>
+        <label htmlFor="username">Username</label>
+        <input id="username" type="text" />
+        <label htmlFor="age">Age (Years)</label>
+        <input id="age" type="number" />
+        <button type="submit">Add User</button>
+      </form>
+    </Card>
+  );
 };
 
 export default AddUser;
